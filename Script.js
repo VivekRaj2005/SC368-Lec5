e9688b4b230ef22fa5ca52dccc1cfc3cbe81e9eb
Vivek Raj <VivekRaj27082005@gmail.com>
2024-11-13 22:16:40 +0530

    Inital

--- Script.js
+++ Script.js
@@ -1,11 +1,5 @@
-const title = document.getElementById("title");
-
-// let age = parseInt(prompt("Whats you age?"));
-
-// if (age > 18) {
-//   title.innerText = "Vote";
-//   title.style.color = "green";
-// } else {
-//   title.innerText = "Cannot Vote";
-//   title.style.color = "red";
-// }
+const inputText = document.getElementById("TodoTitle");
+function onClickSubmitButton(event) {
+  event.preventDefault();
+  console.log(inputText);
+}
