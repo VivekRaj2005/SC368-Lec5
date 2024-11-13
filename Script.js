31e288a4b57f69a79ffaa8488720d8ae4501e9ca
Vivek Raj <VivekRaj27082005@gmail.com>
2024-11-13 23:05:19 +0530

    Inital

--- Script.js
+++ Script.js
@@ -1,5 +1,23 @@
 const inputText = document.getElementById("TodoTitle");
+const list = document.getElementById("todos");
+const countdown = document.getElementsByClassName("countdown")[0];
+let initalCount = 10;
+const timer = setInterval(Countdown, 1000);
+
+function Countdown() {
+  initalCount -= 1;
+  countdown.innerText = initalCount;
+  if (initalCount < 0) {
+    clearInterval(timer);
+    countdown.innerText = "Timer Done!!!";
+  }
+}
+
 const Todos = [];
 function onClickSubmitButton() {
-  console.log(inputText.value);
+  Todos.push(inputText.value);
+  inputText.value = "";
+  Todos.forEach((todo) => {
+    list.innerHTML += "<li>" + todo + "</li>";
+  });
 }
